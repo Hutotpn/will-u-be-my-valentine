503bd23808b9c49bb3195d8d1cc4394ae91f5ed9
Huto <92351427+Hutotpn@users.noreply.github.com>
2024-02-08 15:29:41 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const yesButton = document.getElementById('yesButton');
+    const noButton = document.getElementById('noButton');
+    const responseMessage = document.getElementById('responseMessage');
+
+    const questions = [
+        "Are you sure?",
+        "Are you really sure??",
+        "Are you really really sure???",
+        "Think again?",
+        "Don't believe in second chances?",
+        "Why are you being so cold?",
+        "Maybe we can talk about it?",
+        "I am not going to ask again!",
+        "Ok now this is hurting my feelings!",
+        "You are now just being mean!",
+        "Why are you doing this to me?",
+        "Please give me a chance!",
+        "I am begging you to stop!"
+    ];
+
+    let questionIndex = 0;
+
+    yesButton.addEventListener('click', function() {
+        responseMessage.innerText = questions[questionIndex];
+        questionIndex = (questionIndex + 1) % questions.length;
+    });
+
+    noButton.addEventListener('click', function() {
+        responseMessage.innerText = questions[questionIndex];
+        questionIndex = (questionIndex + 1) % questions.length;
+    });
+});
